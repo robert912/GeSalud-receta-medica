3adb71ef12099b7621c92d93836a34f242a03894
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2025-06-17 17:55:00 -0400

    Primer commit

---
+++ src/main/java/com/gesalud/receta_medica/RecetaMedicaGesaludApplication.java
@@ -0,0 +1,13 @@
+package com.gesalud.receta_medica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetaMedicaGesaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetaMedicaGesaludApplication.class, args);
+	}
+
+}
